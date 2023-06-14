875096b4355795665e40b48f1d1a5d62dfc751ea
Taiwo Bukola <122969049+Taiwof@users.noreply.github.com>
2023-06-13 22:11:02 -0700

    Update 4-hidden_discovery.py

--- 0x02-python-import_modulules/4-hidden_discovery.py
+++ 0x02-python-import_modulules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-import hidden_4
-words = dir(hidden_4)
-for word in words:
-if (word[:2] != '__'):
-print(f"{word:s}")
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
