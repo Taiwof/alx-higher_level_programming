bb3b020f8cc05e7dac3ba82cba4af32aeb346e65
Taiwo Bukola <122969049+Taiwof@users.noreply.github.com>
2023-06-13 22:08:48 -0700

    Update 2-args.py

--- 0x02-python-import_modulules/2-args.py
+++ 0x02-python-import_modulules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    words = sys.argv[1:]
-    word = len(words)
 
-    if word == 0:
-        print('{} arguments.'.format("0"))
-    elif word == 1:
-        print('{} argument:'.format(word))
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(word))
-
-    for i in range(word):
-        print('{}: {}'.format(i + 1, words[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
